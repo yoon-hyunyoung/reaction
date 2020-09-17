8a944f6b0ca9de24b2902754f0e5305789bfdbd6
yoonhyunyoung <taupo28@gmail.com>
2020-09-17 16:06:02 +0900

    Count만들어서 원격(?) 사용 개발

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import React from 'react';
 import 'App.css';
 import 'antd/dist/antd.css';
 import {Button} from 'antd';
+import Count from 'Count';
 
-function Count() {
-  const [cnt, setCnt] = React.useState(0)
-  const click = (event) => {
-    setCnt(cnt + 1) 
-  }
-  return (
-    <div>
-      합계숫자는? <span>{cnt}</span>
-      <div onClick={click}>클릭</div>    
-    </div>
-  )
-}
+
 
 function App() {
   return (
---
+++ src/Count.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Count() {
+  const [cnt, setCnt] = React.useState(0);
+  const click23 = (event) => {
+    setCnt(cnt + 1);
+  }
+  return (
+    <div>
+      합계숫자는? <span>{cnt}</span>
+      <div onClick={click23}>클릭</div>    
+    </div>
+  );
+}
+
+
+export default Count;
