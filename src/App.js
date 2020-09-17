8e513b8b278bd54b10957be8710c3c6cf462620d
yoonhyunyoung <taupo28@gmail.com>
2020-09-17 15:56:58 +0900

    리액트  dom, 클릭이벤트 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'App.css';
+import 'antd/dist/antd.css';
+import {Button} from 'antd';
+
+function Count() {
+  const [cnt, setCnt] = React.useState(0)
+  const click = (event) => {
+    setCnt(cnt + 1) 
+  }
+  return (
+    <div>
+      합계숫자는? <span>{cnt}</span>
+      <div onClick={click}>클릭</div>    
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='content'>
+      <Count/>
     </div>
   );
 }
