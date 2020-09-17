e94ddb08e4d0ecff2717afd9baa8c0a154127b44
yoonhyunyoung <taupo28@gmail.com>
2020-09-17 18:39:18 +0900

    App 순서 변경

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import 'App.css';
 import 'antd/dist/antd.css';
 import {Button} from 'antd';
-// import Count from 'Count';
-// import Act from 'Act';
-// import Welcome from 'Welcome';
+import Count from 'Count';
+import Act from 'Act';
+import Welcome from 'Welcome';
 import Bbutton from 'Bbutton';
 
 const  x = [2, 3, 4, 5, 7]
@@ -14,9 +14,9 @@ console.log("평균자책점으로 응용 가능", map);
 function App() {
   return (
     <div>
-      {/* <Count/>
+      <Count/>
+      <Welcome/>
       <Act/>
-      <Welcome/> */}
       <Bbutton/>
     </div>
   );
