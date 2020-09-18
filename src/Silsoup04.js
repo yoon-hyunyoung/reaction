6245e12b05cd34afad2b1f85e3dfdd693e02a1e0
yoonhyunyoung <taupo28@gmail.com>
2020-09-18 16:54:46 +0900

    실습 범위 10에서 9로도괴서 9로 단순수정

--- src/Silsoup04.js
+++ src/Silsoup04.js
@@ -8,7 +8,7 @@ function Silsoup04() {
     const [num, setNum] = React.useState([1,2,3,4,5,6,7,8,9]);
     const click = (event) => {
         console.log("클릭")
-        setNum([...num.slice(0, 4), 0, 0, ...num.slice(6, 10)])
+        setNum([...num.slice(0, 4), 0, 0, ...num.slice(6, 9)])
         }
 
     return (
