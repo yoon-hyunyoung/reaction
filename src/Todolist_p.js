56dd1dff8e690bf7c9e8dac58404f75553c8ee82
yoonhyunyoung <taupo28@gmail.com>
2020-09-18 19:29:08 +0900

    todolist 힌트 얻음(선생님 바쁘심 ㅋㅋ)

--- src/Todolist_p.js
+++ src/Todolist_p.js
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
 import Todolist_c from 'Todolist_c'
 function Todolist_p()
-{   const [cnt, setCnt] = useState(0) => {
-    cnt += 1
-    }
-    const [text, texts] = useState('입력값') => {
-        ????
-    }
+{   
+    const [cnt, setCnt] = useState(0);
+     // [] 리스트
+    const [text, texts] = useState('입력값');
 
 return (
     <>
         <div>todo list Component<br/>
             <input box></input>
             <button>추가</button>
+             {/* map으로 반복*/}
             <Todolist_c
                 countingnum = {cnt}
                 inputdata = {???}
             />
-
-
-
-
-            
         </div>
     </>
 );
