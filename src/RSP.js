dd456a19e542b2847a31997e6e7aed4c18a629cc
yoonhyunyoung <taupo28@gmail.com>
2020-09-22 11:05:50 +0900

    그림도 랜덤 되는 오류 수정

--- src/RSP.js
+++ src/RSP.js
@@ -4,7 +4,7 @@ import img2 from "C:/Users/ASIA_04/Desktop/todo_react/todo/src/2.png"
 import img3 from "C:/Users/ASIA_04/Desktop/todo_react/todo/src/3.png"
 
 export default function RSP(){
-    
+    const[comimg, setComimg] = useState();
     const[imgs, setImgs] = useState();
     const[user, setUser] = useState();
     const[ment, setMent] = useState("결과는??");
@@ -19,19 +19,19 @@ export default function RSP(){
             console.log("draw")
             setCom('가위')
             setMent('비겼습니다')
-            setImgs(img1)
+            setComimg(img1)
         }
-        if (com==2){
+        else if (com==2){
             console.log("lose")
             setCom('바위')
             setMent('졌습니다')
-            setImgs(img2)
+            setComimg(img2)
         }
-        if (com==3){
+        else if (com==3){
             console.log("win")
             setCom('보')
             setMent('이겼습니다')
-            setImgs(img3)
+            setComimg(img3)
         }
     }
     const click1 = () => {
@@ -44,19 +44,19 @@ export default function RSP(){
             console.log("win")
             setCom('가위')
             setMent('이겼습니다')
-            setImgs(img1)
+            setComimg(img1)
         }
         else if (com==2){
             console.log("draw")
             setCom('바위')
             setMent('비겼습니다')
-            setImgs(img2)
+            setComimg(img2)
         }
         else if (com==3){
             console.log("lose")
             setCom('보')
             setMent('졌습니다')
-            setImgs(img3)
+            setComimg(img3)
         }
     }
     const click2 = () => {
@@ -69,19 +69,19 @@ export default function RSP(){
             console.log("lose")
             setCom('가위')
             setMent('졌습니다')
-            setImgs(img1)
+            setComimg(img1)
         }
-        if (com==2){
+        else if (com==2){
             console.log("win")
             setCom('바위')
             setMent('이겼습니다')
-            setImgs(img2)
+            setComimg(img2)
         }
-        if (com==3){
+        else if (com==3){
             console.log("draw")
             setCom('보')
             setMent('비겼습니다')
-            setImgs(img3)
+            setComimg(img3)
         }
     }
     const [cnt, setCnt] = useState(-1);
@@ -94,7 +94,7 @@ export default function RSP(){
                 <p  style={{fontSize:70}}>셋 중 하나를 선택해 주세요.</p>
                 <span style={{fontSize:50}}>{cnt}</span><span  style={{fontSize:30}}>회 진행</span>
                 
-                <p  style={{fontSize:50}}>com :<img src={imgs}/> {com} <br/>user :<img src={imgs}/> {user}</p>
+                <p  style={{fontSize:50}}>com :<img src={comimg}/> {com} <br/>user :<img src={imgs}/> {user}</p>
 
                 <div style={{overflow:0}}><img title='가위선택' src={img1} onClick={click}/>
                 <img title='바위선택' src={img2} onClick={click1}/>
